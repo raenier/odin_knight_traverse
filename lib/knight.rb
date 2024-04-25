fd2850482370efb62ca865ad130ad5587eb29040
ren <raenierquiambao@gmail.com>
2024-04-25 16:15:34 +0800

    Create moves variable for knight pattern moves
    - freeze constant variables
    - constant move is for determining possible moves of knight

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class Knight
-  BOARD = [7, 7]
+  BOARD = [7, 7].freeze
+  MOVES = [
+    [2, 1],
+    [2, -1],
+    [1, 2],
+    [1, -2],
+    [-1, -2],
+    [-1, 2],
+    [-2, 1],
+    [-2, -1]
+  ].freeze
 
   attr_accessor :position
 
