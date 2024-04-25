3a659fd9b2f4543d907655ba34d2fa8703ff3e6f
ren <raenierquiambao@gmail.com>
2024-04-25 16:19:10 +0800

    Determine possible positions for knight
    - determine possible positions based on current position and board size
    - do not go over the edge of the board
    - use constant Move for knight pattern movement

--- lib/knight.rb
+++ lib/knight.rb
@@ -18,4 +18,18 @@ class Knight
   def initialize(y_start = 0, x_start = 0)
     self.position = [y_start, x_start]
   end
+
+  def possible_positions
+    positions = []
+
+    MOVES.each do |ymove, xmove|
+      possible_y = position[0] + ymove
+      possible_x = position[1] + xmove
+      next if possible_x.negative? || possible_x > 7 || possible_y.negative? || possible_y > 7
+
+      positions << [possible_y, possible_x]
+    end
+
+    positions
+  end
 end
