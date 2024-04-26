ba1412a8a652dfe59f352d047fbe9d5928de59f5
ren <raenierquiambao@gmail.com>
2024-04-26 16:41:04 +0800

    Return shortest path from current position to given position
    - Store parents of position to hash
    - Recreate path based on parents

--- lib/knight.rb
+++ lib/knight.rb
@@ -38,12 +38,31 @@ class Knight
 
     queue = possible_positions
     given_position = [yinput, xinput]
+    parents = {}
+    parents[position] = possible_positions
 
     until queue.empty?
-      current_position = queue
-      return current_position if current_position == given_position
+      current_position = queue.shift
+      return recreate_path(current_position, parents) + given_position if current_position == given_position
 
-      queue << possible_positions(current_position)
+      parents[current_position] = possible_positions(current_position)
+      queue += parents[current_position]
     end
+  end
+
+  def recreate_path(current_position, parents)
+    path = []
+    until current_position == position
+      parents.each do |parent, children|
+        if children.include? current_position
+          current_position = parent
+          parents.delete parent
+          path << current_position
+          #if found break out of this loop do not loop further
+          break
+        end
+      end
+    end
+    path.reverse
   end
 end
